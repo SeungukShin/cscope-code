32f5d8f82747232b73ea409d13ba630938142b54
Seunguk Shin <seunguk.shin@gmail.com>
2020-09-02 22:21:19 +0900

    Split CscopeItem class

--- src/cscopeQuery.ts
+++ src/cscopeQuery.ts
@@ -4,65 +4,11 @@ import * as rl from 'readline';
 import { CscopeExecute } from './cscopeExecute';
 import { CscopeConfig } from './cscopeConfig';
 import { CscopeLog } from './cscopeLog';
+import { CscopeItem } from './cscopeItem';
 import { CscopePosition } from './cscopePosition';
 import { resolve } from 'dns';
 import { rejects } from 'assert';
 import { syncBuiltinESMExports } from 'module';
-
-class CscopeItem implements vscode.QuickPickItem, vscode.CallHierarchyItem {
-	private rest: string;
-	private text: string;
-	// for QuickPickItem
-	label: string;
-	// for CallHierarchyItem
-	detail: string;
-	kind: vscode.SymbolKind;
-	name: string;
-	range: vscode.Range;
-	selectionRange: vscode.Range;
-	uri: vscode.Uri;
-
-	constructor(uri: vscode.Uri, func: string, range: vscode.Range, rest: string, text: string) {
-		const offset = vscode.workspace.rootPath ? vscode.workspace.rootPath.length + 1 : 0;
-		this.uri = uri;
-		this.name = func;
-		this.rest = rest;
-		this.text = text;
-		this.label = func + ' : ' + rest;
-		this.detail = uri.fsPath.substring(offset) + ':' + range.start.line.toString() + ':' + range.start.character.toString();
-		this.kind = vscode.SymbolKind.Function;
-		this.range = range;
-		this.selectionRange = range;
-	}
-
-	getUri(): vscode.Uri {
-		return this.uri;
-	}
-
-	getFile(): string {
-		return this.uri.fsPath;
-	}
-
-	getFunction(): string {
-		return this.name;
-	}
-
-	getRange(): vscode.Range {
-		return this.range;
-	}
-
-	getLineNumber(): number {
-		return this.range.start.line;
-	}
-
-	getColumnNumber(): number {
-		return this.range.start.character;
-	}
-
-	getLine(): string {
-		return this.rest;
-	}
-}
 
 export class CscopeQuery {
 	private config: CscopeConfig;
